58a02c4ce815365ec5c86dbaa492165f6be0644c
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-08-24 16:28:01 +0200

    test: Add integration tests with 0% and 100% fee

--- contracts/locked-astroport-vault/tests/common.rs
+++ contracts/locked-astroport-vault/tests/common.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use cosmwasm_std::Coin;
+use cosmwasm_std::{Coin, Decimal};
 use cw_it::cw_multi_test::{StargateKeeper, StargateMessageHandler};
 use cw_it::multi_test::modules::TokenFactory;
 use cw_it::multi_test::MultiTestRunner;
@@ -18,6 +18,12 @@ pub const DENOM_CREATION_FEE: &str = "10000000uosmo";
 
 const TOKEN_FACTORY: &TokenFactory =
     &TokenFactory::new("factory", 32, 16, 59 + 16, DENOM_CREATION_FEE);
+
+/// An enum to represent the different default vault setups
+pub enum VaultSetup {
+    WstEth,
+    AxlrNtrn,
+}
 
 pub fn get_test_runner<'a>() -> TestRunner<'a> {
     match option_env!("TEST_RUNNER_TYPE").unwrap_or("multi-test") {
@@ -49,6 +55,30 @@ pub fn default_instantiate<'a>(
         vault_contract,
         token_factory_fee,
         treasury_addr.address(),
+        Decimal::percent(5),
+        dependencies,
+        admin,
+    );
+
+    (robot, treasury_addr)
+}
+
+pub fn instantiate_wsteth_eth_vault<'a>(
+    runner: &'a TestRunner<'a>,
+    admin: &SigningAccount,
+    performance_fee: Decimal,
+    dependencies: &'a LockedVaultDependencies<'a>,
+) -> (LockedAstroportVaultRobot<'a>, SigningAccount) {
+    let vault_contract = LockedAstroportVaultRobot::contract(runner, UNOPTIMIZED_PATH);
+    let treasury_addr = runner.init_account(&[]).unwrap();
+    let token_factory_fee = Coin::from_str(DENOM_CREATION_FEE).unwrap();
+
+    let robot = LockedAstroportVaultRobot::new_wsteth_eth_vault(
+        runner,
+        vault_contract,
+        token_factory_fee,
+        treasury_addr.address(),
+        performance_fee,
         dependencies,
         admin,
     );
@@ -59,6 +89,7 @@ pub fn default_instantiate<'a>(
 pub fn instantiate_axlr_ntrn_vault<'a>(
     runner: &'a TestRunner<'a>,
     admin: &SigningAccount,
+    performance_fee: Decimal,
     dependencies: &'a LockedVaultDependencies<'a>,
 ) -> (LockedAstroportVaultRobot<'a>, SigningAccount) {
     let vault_contract = LockedAstroportVaultRobot::contract(runner, UNOPTIMIZED_PATH);
@@ -71,9 +102,27 @@ pub fn instantiate_axlr_ntrn_vault<'a>(
             vault_contract,
             token_factory_fee,
             treasury_addr.address(),
+            performance_fee,
             dependencies,
             admin,
         );
 
     (robot, treasury_addr)
+}
+
+pub fn instantiate_vault<'a>(
+    runner: &'a TestRunner<'a>,
+    admin: &SigningAccount,
+    setup: VaultSetup,
+    performance_fee: Decimal,
+    dependencies: &'a LockedVaultDependencies<'a>,
+) -> (LockedAstroportVaultRobot<'a>, SigningAccount) {
+    match setup {
+        VaultSetup::WstEth => {
+            instantiate_wsteth_eth_vault(runner, admin, performance_fee, dependencies)
+        }
+        VaultSetup::AxlrNtrn => {
+            instantiate_axlr_ntrn_vault(runner, admin, performance_fee, dependencies)
+        }
+    }
 }
