150e1f7dd16979a41eef36b0581db3a1ec1cda75
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-08-25 18:31:35 +0200

    test: Add test for correct event on unlock

--- contracts/locked-astroport-vault/tests/test_lockup.rs
+++ contracts/locked-astroport-vault/tests/test_lockup.rs
@@ -1,6 +1,13 @@
 use common::{default_instantiate, get_test_runner, DEPS_PATH};
+use cosmwasm_std::{attr, coin, Uint128};
+use cw_it::robot::TestRobot;
 use cw_it::test_tube::Account;
+use cw_vault_standard::extensions::lockup::{
+    UNLOCKING_POSITION_ATTR_KEY, UNLOCKING_POSITION_CREATED_EVENT_TYPE,
+};
 use cw_vault_standard_test_helpers::traits::force_unlock::ForceUnlockVaultRobot;
+use cw_vault_standard_test_helpers::traits::CwVaultStandardRobot;
+use locked_astroport_vault::msg::ExecuteMsg;
 use locked_astroport_vault_test_helpers::robot::LockedAstroportVaultRobot;
 
 pub mod common;
@@ -16,4 +23,42 @@ fn cannot_add_and_remove_the_same_address_to_force_withdraw_whitelist() {
     let user = robot.new_user(&admin);
 
     robot.update_force_withdraw_whitelist(&admin, vec![user.address()], vec![user.address()]);
+}
+
+#[test]
+fn unlocking_position_event_emitted_when_vault_has_lockup() {
+    let runner = get_test_runner();
+    let admin = LockedAstroportVaultRobot::new_admin(&runner);
+    let dependencies = LockedAstroportVaultRobot::instantiate_deps(&runner, &admin, DEPS_PATH);
+    let (robot, _treasury) = default_instantiate(&runner, &admin, &dependencies);
+    let user = robot.new_user(&admin);
+
+    let deposit_amount = Uint128::new(100);
+    let vault_token_balance = robot
+        .deposit_cw20(deposit_amount, None, &user)
+        .query_vault_token_balance(user.address());
+
+    let res = robot
+        .wasm()
+        .execute(
+            &robot.vault_addr(),
+            &ExecuteMsg::Redeem {
+                amount: vault_token_balance,
+                recipient: None,
+            },
+            &[coin(vault_token_balance.u128(), robot.vault_token())],
+            &user,
+        )
+        .unwrap();
+    for event in res.events.iter() {
+        println!("{:?}", event);
+    }
+    res.events
+        .iter()
+        .find(|e| {
+            e.ty == format!("wasm-{}", UNLOCKING_POSITION_CREATED_EVENT_TYPE)
+                && e.attributes
+                    .contains(&attr(UNLOCKING_POSITION_ATTR_KEY, "0"))
+        })
+        .unwrap();
 }
