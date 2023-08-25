73ebda71f14642208261862e511eeff2808e0480
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-08-25 16:47:28 +0200

    test: Add failing property test: "minted_vault_token_amount_decreases_as_rewards_compound"

---
+++ contracts/locked-astroport-vault/tests/common/compound.rs
@@ -0,0 +1,68 @@
+use cosmwasm_std::{Decimal, Uint128};
+use cw_it::robot::TestRobot;
+use cw_it::test_tube::{Account, SigningAccount};
+use cw_vault_standard_test_helpers::traits::CwVaultStandardRobot;
+use locked_astroport_vault_test_helpers::robot::LockedAstroportVaultRobot;
+
+/// Desposits `deposit_amount` base tokens into the vault, donates some of each
+/// reward token to the vault, then compounds the vault and asserts that the
+/// base token balance corresponding to the users vault token balance has
+/// increased.
+pub fn test_compound_vault(
+    robot: &LockedAstroportVaultRobot,
+    deposit_amount: Uint128,
+    fee: Decimal,
+    user: &SigningAccount,
+    admin: &SigningAccount,
+) {
+    // Check vault token balance and this amount converted to base tokens before
+    // deposit
+    let vt_balance_before_deposit = robot.query_vault_token_balance(user.address());
+    let bt_balance_in_vault_before_deposit =
+        robot.query_convert_to_assets(vt_balance_before_deposit);
+
+    // Deposit some funds and assert the vault token balance is correct
+    let vt_balance_after_deposit = robot
+        .deposit_cw20(deposit_amount, None, &user)
+        .assert_vt_balance_converted_to_assets_eq(
+            user.address(),
+            bt_balance_in_vault_before_deposit + deposit_amount,
+        )
+        .query_vault_token_balance(user.address());
+
+    // Donate some reward tokens to the vault to simulate rewards accruing, then
+    // compound and check that the base token amount corresponding to the users
+    // vault token balance has increased.
+    let config = robot.query_config();
+    let treasury = config.treasury;
+    let reward_tokens = config.reward_tokens;
+    let mut base_token_balance_in_vault = bt_balance_in_vault_before_deposit + deposit_amount;
+    for token in reward_tokens {
+        println!("Donating {}", token);
+        let donation_amount = Uint128::new(1_000_000);
+
+        let amount_in_treasury_before =
+            robot.query_native_token_balance(treasury.to_string(), token.to_string());
+
+        robot
+            .send_native_tokens(admin, &robot.vault_addr, donation_amount, token.to_string())
+            .assert_vt_balance_converted_to_assets_eq(user.address(), base_token_balance_in_vault)
+            .compound_vault(&user)
+            .assert_native_token_balance_eq(
+                treasury.to_string(),
+                token.to_string(),
+                amount_in_treasury_before + donation_amount * fee,
+            );
+
+        // If fee is less than 100% then the users base token balance in the vault
+        // should have increased
+        if fee < Decimal::percent(100) {
+            robot.assert_vt_balance_converted_to_assets_gt(
+                user.address(),
+                base_token_balance_in_vault,
+            );
+        }
+
+        base_token_balance_in_vault = robot.query_convert_to_assets(vt_balance_after_deposit);
+    }
+}
--- contracts/locked-astroport-vault/tests/test_compound.rs
+++ contracts/locked-astroport-vault/tests/test_compound.rs
@@ -1,53 +1,13 @@
 use common::{instantiate_vault, VaultSetup, DEPS_PATH};
 use cosmwasm_std::{Decimal, Uint128};
-use cw_it::robot::TestRobot;
-use cw_it::test_tube::{Account, SigningAccount};
+use cw_it::test_tube::Account;
 use cw_vault_standard_test_helpers::traits::CwVaultStandardRobot;
 use locked_astroport_vault_test_helpers::robot::LockedAstroportVaultRobot;
 use test_case::test_case;
 
 pub mod common;
-pub use common::{get_test_runner, UNOPTIMIZED_PATH};
-
-fn test_compound_vault(robot: &LockedAstroportVaultRobot, fee: Decimal, admin: &SigningAccount) {
-    let user = robot.new_user(admin);
-
-    // Deposit some funds and assert the vault token balance is correct
-    let base_token_balance = robot.query_base_token_balance(user.address());
-    let deposit_amount = base_token_balance / Uint128::new(2);
-    robot
-        .deposit_cw20(deposit_amount, None, &user)
-        .assert_vault_token_balance_eq(user.address(), deposit_amount)
-        .assert_vt_balance_converted_to_assets_eq(user.address(), deposit_amount);
-
-    // Donate some reward tokens to the vault to simulate rewards accruing, then
-    // compound and check that the base token amount corresponding to the users
-    // vault token balance has increased.
-    let config = robot.query_config();
-    let treasury = config.treasury;
-    let reward_tokens = config.reward_tokens;
-    let mut base_token_balance_in_vault = deposit_amount;
-    for token in reward_tokens {
-        println!("Donating {}", token);
-        let amount = Uint128::new(1_000_000);
-        robot
-            .send_native_tokens(admin, &robot.vault_addr, amount, token.to_string())
-            .assert_vt_balance_converted_to_assets_eq(user.address(), base_token_balance_in_vault)
-            .compound_vault(&user)
-            .assert_native_token_balance_eq(treasury.to_string(), token.to_string(), amount * fee);
-
-        // If fee is less than 100% then the users base token balance in the vault
-        // should have increased
-        if fee < Decimal::percent(100) {
-            robot.assert_vt_balance_converted_to_assets_gt(
-                user.address(),
-                base_token_balance_in_vault,
-            );
-        }
-
-        base_token_balance_in_vault = robot.query_convert_to_assets(deposit_amount);
-    }
-}
+use common::compound::test_compound_vault;
+use common::get_test_runner;
 
 #[test_case(VaultSetup::WstEth, Decimal::percent(5); "Compound wsteth_eth vault, 5% fee.")]
 #[test_case(VaultSetup::AxlrNtrn, Decimal::percent(5); "Compound axlr_ntrn vault, 5% fee.")]
@@ -60,6 +20,10 @@ fn compound_vault(setup: VaultSetup, fee: Decimal) {
     let admin = LockedAstroportVaultRobot::new_admin(&runner);
     let dependencies = LockedAstroportVaultRobot::instantiate_deps(&runner, &admin, DEPS_PATH);
     let (robot, _treasury) = instantiate_vault(&runner, &admin, setup, fee, &dependencies);
+    let user = robot.new_user(&admin);
 
-    test_compound_vault(&robot, fee, &admin)
+    let base_token_balance = robot.query_base_token_balance(user.address());
+    let deposit_amount = base_token_balance / Uint128::new(2);
+
+    test_compound_vault(&robot, deposit_amount, fee, &user, &admin)
 }
