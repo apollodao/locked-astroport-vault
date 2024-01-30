de6ef51a4d718852bfa40d9a0a291e0cfece07ae
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2024-01-30 18:58:07 +0100

    test: deposit fee with multiple recipients

--- contracts/locked-astroport-vault/tests/test_deposit.rs
+++ contracts/locked-astroport-vault/tests/test_deposit.rs
@@ -105,5 +105,50 @@ fn deposit_fee_works() {
         .deposit_cw20(deposit_amount, None, Unwrap::Ok, &user)
         .assert_vault_token_balance_eq(user.address(), expected_amount)
         .assert_total_vault_token_supply_eq(expected_amount)
-        .assert_total_vault_assets_eq(deposit_amount * (Decimal::one() - fee_rate));
+        .assert_total_vault_assets_eq(deposit_amount * (Decimal::one() - fee_rate))
+        .assert_base_token_balance_eq(treasury.address(), deposit_amount * fee_rate);
+}
+
+#[test]
+fn deposit_fee_works_with_multiple_recipients() {
+    let owned_runner = get_test_runner();
+    let runner = owned_runner.as_ref();
+    let admin = runner
+        .init_account(&Coins::from_str(DEFAULT_COINS).unwrap().to_vec())
+        .unwrap();
+    let dependencies = LockedAstroportVaultRobot::instantiate_deps(&runner, &admin, DEPS_PATH);
+    let treasury_one = runner.init_account(&[]).unwrap();
+    let treasury_two = runner.init_account(&[]).unwrap();
+    let fee_rate = Decimal::percent(5);
+    let deposit_fee = Some(FeeConfig {
+        fee_rate,
+        fee_recipients: vec![
+            (treasury_one.address(), Decimal::percent(80)),
+            (treasury_two.address(), Decimal::percent(20)),
+        ],
+    });
+    let robot =
+        instantiate_wsteth_eth_vault(&runner, &admin, None, deposit_fee, None, &dependencies);
+    let user = robot.new_user(&admin);
+
+    // Deposit some funds and assert the vault token balance is correct
+    let base_token_balance = robot.query_base_token_balance(user.address());
+    let deposit_amount = base_token_balance / Uint128::new(2);
+    let expected_amount =
+        deposit_amount * (Decimal::one() - fee_rate) * INITIAL_VAULT_TOKENS_PER_BASE_TOKEN;
+    robot
+        .deposit_cw20(deposit_amount, None, Unwrap::Ok, &user)
+        .assert_vault_token_balance_eq(user.address(), expected_amount)
+        .assert_total_vault_token_supply_eq(expected_amount)
+        .assert_total_vault_assets_eq(deposit_amount * (Decimal::one() - fee_rate))
+        .assert_base_token_balance_eq(
+            treasury_one.address(),
+            deposit_amount * fee_rate * Decimal::percent(80),
+        )
+        .assert_base_token_balance_eq(
+            treasury_two.address(),
+            deposit_amount * fee_rate * Decimal::percent(20),
+        );
+}
+
 }
