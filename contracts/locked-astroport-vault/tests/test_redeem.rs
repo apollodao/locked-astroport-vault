391672161ca09e2d550328a7eda5fddafe94d0f8
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2024-02-02 19:22:56 +0100

    test: Add tests for withdrawal fee with multiple recipients

--- contracts/locked-astroport-vault/tests/test_redeem.rs
+++ contracts/locked-astroport-vault/tests/test_redeem.rs
@@ -161,6 +161,61 @@ fn withdrawal_fee_works_without_lockup() {
 }
 
 #[test]
+fn withdrawal_fee_with_multiple_recipients_works_without_lockup() {
+    let owned_runner = get_test_runner();
+    let runner = owned_runner.as_ref();
+    let admin = runner
+        .init_account(&Coins::from_str(DEFAULT_COINS).unwrap().to_vec())
+        .unwrap();
+    let dependencies = LockedAstroportVaultRobot::instantiate_deps(&runner, &admin, DEPS_PATH);
+    let treasury_one = runner.init_account(&[]).unwrap();
+    let treasury_two = runner.init_account(&[]).unwrap();
+    let fee_rate: Decimal = Decimal::percent(1);
+    let withdrawal_fee = Some(FeeConfig {
+        fee_rate,
+        fee_recipients: vec![
+            (treasury_one.address(), Decimal::percent(80)),
+            (treasury_two.address(), Decimal::percent(20)),
+        ],
+    });
+    let robot =
+        instantiate_axlr_ntrn_vault(&runner, &admin, None, None, withdrawal_fee, &dependencies);
+    let user = robot.new_user(&admin);
+
+    // Deposit some funds, assert that vt and bt balances are correct, redeem, and
+    // assert again. Assert user's and treasury's balances are correct.
+    let base_token_balance = robot.query_base_token_balance(user.address());
+    let deposit_amount = Uint128::new(100);
+    robot
+        .deposit_cw20(deposit_amount, None, Unwrap::Ok, &user)
+        .assert_vault_token_balance_eq(
+            user.address(),
+            deposit_amount * INITIAL_VAULT_TOKENS_PER_BASE_TOKEN,
+        )
+        .assert_base_token_balance_eq(user.address(), base_token_balance - deposit_amount)
+        .redeem(
+            deposit_amount * INITIAL_VAULT_TOKENS_PER_BASE_TOKEN,
+            None,
+            Unwrap::Ok,
+            None,
+            &user,
+        )
+        .assert_base_token_balance_eq(
+            user.address(),
+            base_token_balance - deposit_amount * fee_rate,
+        )
+        .assert_vault_token_balance_eq(user.address(), Uint128::zero())
+        .assert_base_token_balance_eq(
+            treasury_one.address(),
+            deposit_amount * fee_rate * Decimal::percent(80),
+        )
+        .assert_base_token_balance_eq(
+            treasury_two.address(),
+            deposit_amount * fee_rate * Decimal::percent(20),
+        );
+}
+
+#[test]
 fn withdrawal_fee_works_with_lockup() {
     let owned_runner = get_test_runner();
     let runner = owned_runner.as_ref();
@@ -198,4 +253,55 @@ fn withdrawal_fee_works_with_lockup() {
             deposit_amount * (Decimal::one() - fee_rate),
         )
         .assert_base_token_balance_eq(treasury.address(), deposit_amount * fee_rate);
+}
+
+#[test]
+fn withdrawal_fee_with_multiple_recipients_works_with_lockup() {
+    let owned_runner = get_test_runner();
+    let runner = owned_runner.as_ref();
+    let admin = runner
+        .init_account(&Coins::from_str(DEFAULT_COINS).unwrap().to_vec())
+        .unwrap();
+    let dependencies = LockedAstroportVaultRobot::instantiate_deps(&runner, &admin, DEPS_PATH);
+    let treasury_one = runner.init_account(&[]).unwrap();
+    let treasury_two = runner.init_account(&[]).unwrap();
+    let fee_rate: Decimal = Decimal::percent(1);
+    let withdrawal_fee = Some(FeeConfig {
+        fee_rate,
+        fee_recipients: vec![
+            (treasury_one.address(), Decimal::percent(30)),
+            (treasury_two.address(), Decimal::percent(70)),
+        ],
+    });
+    let robot =
+        instantiate_wsteth_eth_vault(&runner, &admin, None, None, withdrawal_fee, &dependencies);
+    let user = robot.new_user(&admin);
+
+    // Deposit some funds and then redeem.
+    let deposit_amount = Uint128::new(100);
+    robot
+        .deposit_cw20(deposit_amount, None, Unwrap::Ok, &user)
+        .redeem(
+            deposit_amount * INITIAL_VAULT_TOKENS_PER_BASE_TOKEN,
+            None,
+            Unwrap::Ok,
+            None,
+            &user,
+        );
+
+    // Assert that the claim was created correctly and that the fee was received
+    robot
+        .assert_unlocking_position_has_props(
+            0,
+            &user.address(),
+            deposit_amount * (Decimal::one() - fee_rate),
+        )
+        .assert_base_token_balance_eq(
+            treasury_one.address(),
+            deposit_amount * fee_rate * Decimal::percent(30),
+        )
+        .assert_base_token_balance_eq(
+            treasury_two.address(),
+            deposit_amount * fee_rate * Decimal::percent(70),
+        );
 }
